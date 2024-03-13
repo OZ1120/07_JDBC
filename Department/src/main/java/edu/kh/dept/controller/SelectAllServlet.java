fc88d991e1ebc2f1d522e7654fc170474fdecc66
OZ <macoto8@naver.com>
2024-03-13 17:03:57 +0900

    240313_JDBC

--- Department/src/main/java/edu/kh/dept/controller/SelectAllServlet.java
+++ Department/src/main/java/edu/kh/dept/controller/SelectAllServlet.java
@@ -1,9 +1,10 @@
 package edu.kh.dept.controller;
 
-import java.io.IOException;
-import java.sql.Connection;
+import java.util.List;
 
-import edu.kh.dept.common.JDBCTemplate;
+import edu.kh.dept.model.dto.Department;
+import edu.kh.dept.model.service.DepartmentService;
+import edu.kh.dept.model.service.DepartmentServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,17 +16,28 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/department/selectAll")
 public class SelectAllServlet extends HttpServlet {
 
-	
+
 	// Get방식 요청 처리
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException{
 		
 		try {
-			Connection conn = JDBCTemplate.getConnection();
 			
-			System.out.println(conn);
+			// Service 객체 생성
+			DepartmentService service = new DepartmentServiceImpl();
 			
-			JDBCTemplate.close(conn);
+			// 모든 부서 조회 Service 호출 후 결과 반환 받기
+			List<Department> deptList = service.selectAll();
+			
+			// DB 조회 결과를 request scope에 세팅하여
+			// JSP로 요청 위임(forward) 하기
+			
+			req.setAttribute("deptList", deptList);
+			
+			String path = "/WEB-INF/views/selectAll.jsp";
+			
+			req.getRequestDispatcher(path).forward(req, resp);
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
